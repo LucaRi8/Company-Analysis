016565adbcb9a5986de3123c641eaf841ff294ef
Luca Riotto <lucariotto709@gmail.com>
2025-02-08 13:49:33 +0100

    add date for sampled data

---
+++ const.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+# date dict for sample time series data
+sampled_ticker_date = {
+    'start_date': '2022-01-01',
+    'end_date': datetime.today().strftime('%Y-%m-%d')
+}
+
+
+

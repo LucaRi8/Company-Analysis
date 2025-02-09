028dc0781e8a3ac69e894c1a6110ef4d81fe32bd
Luca Riotto <lucariotto709@gmail.com>
2025-02-09 17:51:58 +0100

    #

--- const.py
+++ const.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 # date dict for sample time series data
 sampled_ticker_date = {
-    'start_date': '2022-01-01',
+    'start_date': '2019-01-01',
     'end_date': datetime.today().strftime('%Y-%m-%d')
 }
 
